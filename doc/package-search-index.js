6218dbc6b8c3b7dbf7e46e91e1b6a7457e4aed80
Maddie Abbaspour <89539144+m-abbaspour@users.noreply.github.com>
2022-08-24 00:11:36 +0430

    initial commit

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"exceptions"},{"l":"manager"},{"l":"utilities"},{"l":"utilitiesTest"}];updateSearchResults();
